37d7544e20b39f79ddbd23bb6fca45bfa7da20b8
ashok135 <ashoksiva135@gmail.com>
2025-09-24 16:10:26 +0530

    add the homecomponets

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,39 +2,45 @@ import { Link } from 'react-router-dom'
 import logo from '../assets/logofooter.png'
 
 const Footermenus = [
-  {
-    menu: "Home",
-    path: "/"
-  }, {
-    menu: "Solution",
-    path: "/#solutions"
-  }, {
-    menu: "What We Do",
-    path: "/#what-we-do"
-  }, {
-    menu: "Contact",
-    path: "/#contact"
-  }
-
+  { menu: "Home", path: "/" },
+  { menu: "Solution", path: "/#solutions" },
+  { menu: "What We Do", path: "/#what-we-do" },
+  { menu: "Contact", path: "/#contact" }
 ]
+
 export const Footer = () => {
   return (
     <footer className="py-5 lg:px-10 px-3 text-20">
       <div className="bg-cream w-full flex justify-between rounded-[35px] py-20 px-28">
-        <div className="w-[1/3">
-          <img src={logo} className='w-[180px]' alt="Vikars academy" />
+        
+        {/* Logo Section */}
+        <div className="w-1/3">
+          <img src={logo} className="w-[180px]" alt="Vikars academy" />
         </div>
-        <div className='w-1/3 space-y-3'>
-          <p><b>Registered Office</b><br />1050 Northgate Dr, Suite #120 San Rafael,<br /> California 94903, USA</p>
+
+        {/* Contact Info */}
+        <div className="w-1/3 space-y-3">
+          <p>
+            <b>Registered Office</b><br />
+            1050 Northgate Dr, Suite #120 San Rafael,<br />
+            California 94903, USA
+          </p>
           <p><b>Email : </b> contact@hekma.ai</p>
-          <p><b>Phone no : </b> +1 415 382 8040</p>
+          <p><b>Phone no : </b> ‪+1 415 382 8040‬</p>
         </div>
-        <div className='w-1/3'>
-          <h5 className='font-bold'>About</h5>
-          {Footermenus.map((item) =>
-            <Link key={item.path} to={item.menu}><h5 className='space-x-3'>{item.menu}</h5></Link>
-          )}
+
+        {/* Menu Links */}
+        <div className="w-1/3">
+          <h5 className="font-bold mb-2">About</h5>
+          <div className="space-y-2">
+            {Footermenus.map((item) => (
+              <Link key={item.path} to={item.path} className="block hover:text-primary">
+                <h5>{item.menu}</h5>
+              </Link>
+            ))}
+          </div>
         </div>
+
       </div>
     </footer>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
@@ -25,6 +26,9 @@ export default {
         '40' : "40px",
         '48' : "48px",
         '64' : "64px",
+      },
+      fontFamily: {
+       inter: ['Inter', 'sans-serif'],
       },
     },
   },
