c605026b059d05c68c6133194620385f9e081a23
thamarai <thamaraiselvig2109@gmail.com>
2025-09-25 16:03:20 +0530

    feat : book demo card created

---
+++ src/components/HomeComponents/BookDemo.tsx
@@ -0,0 +1,7 @@
+export const BookDemo = () => {
+  return (
+    <div>
+      bookdemo
+    </div>
+  )
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Banner } from "../components/HomeComponents/Banner"
+import { BookDemo } from "../components/HomeComponents/BookDemo"
 import { Faqs } from "../components/HomeComponents/Faqs"
  
 export const Home = () => {
@@ -6,6 +7,7 @@ export const Home = () => {
     <>
       <Banner/>
       <Faqs />
+      <BookDemo/>
     </>
   )
 }
