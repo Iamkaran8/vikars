f2b6eb5e282ccded1977072937193732304e8253
thamarai <thamaraiselvig2109@gmail.com>
2025-09-25 16:46:49 +0530

    Book demo feature done with responsiveness

--- src/components/HomeComponents/BookDemo.tsx
+++ src/components/HomeComponents/BookDemo.tsx
@@ -1,7 +1,28 @@
 export const BookDemo = () => {
-  return (
-    <div>
-      bookdemo
-    </div>
-  )
+    return (
+        <>
+            <div className="px-3 md:px-10 lg:px-28 py-10 lg:pb-20">
+                <div className="bg-mint p-6 md:p-10 lg:p-16 flex flex-col lg:flex-row justify-between items-center gap-8 lg:gap-12">
+
+                    {/* Left Side (Heading) */}
+                    <div className="w-full lg:w-2/5 text-forest font-bold text-3xl md:text-5xl lg:text-6xl leading-tight text-center lg:text-left">
+                        Book Your <br /> Demo Class
+                    </div>
+
+                    {/* Right Side (Content) */}
+                    <div className="w-full lg:w-3/5 text-center lg:text-left">
+                        <p className="font-semibold">Still Thinking? Let’s Talk!</p>
+                        <p className="py-4 md:py-5">
+                            Experience Vikar’s with a Free 30-Minute Live Demo Class. <br />
+                            No obligation. No spam. Just honest teaching.
+                        </p>
+                        <button className="w-full md:w-[70%] lg:w-[60%] px-4 py-3 bg-forest text-white rounded-md font-medium">
+                            Book Your Free Demo Now
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
 };
--- src/components/HomeComponents/Faqs.tsx
+++ src/components/HomeComponents/Faqs.tsx
@@ -6,23 +6,21 @@ interface AccordionItemTypes {
 }
 
 const AccordionItem: AccordionItemTypes[] = [
-    { question: " Is this one-on-one or group tuition?", answer: "Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus," },
-    { question: " Is this one-on-one or group tuition?", answer: "Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus," },
-    { question: " Is this one-on-one or group tuition?", answer: "Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus," },
-    { question: " Is this one-on-one or group tuition?", answer: "Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus," },
+    { question: "Is this one-on-one or group tuition?", answer: "Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus," },
+    { question: "Is this one-on-one or group tuition?", answer: "Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus," },
+    { question: "Is this one-on-one or group tuition?", answer: "Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus," },
+    { question: "Is this one-on-one or group tuition?", answer: "Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus," },
 ]
 export const Faqs = () => {
     const [openIndex, setOpenIndex] = useState<number | null>(0);
     return (
         <>
-            <div className="flex flex-col lg:flex-row px-3 md:px-10 lg:px-28 py-10 lg:py-20 gap-10" >
+            <div className="flex flex-col lg:flex-row px-3 md:px-10 lg:px-28 py-5 gap-10" >
 
                 {/* information container */}
                 <div className="w-full lg:w-1/2 flex flex-col justify-between lg:mb-8">
                     <div>
-                        <h3 className="text-forest text-28 md:text-38 lg:text-48 leading-none mb-5 font-bold text-center lg:text-start">
-                            Common Questions Parents Ask
-                        </h3>
+                        <h3 className="text-forest text-28 md:text-38 lg:text-48 leading-none mb-5 font-bold text-center lg:text-start">Common Questions Parents Ask</h3>
                         <p className="text-center lg:text-start">Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
                             eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, </p>
                     </div>
@@ -33,7 +31,7 @@ export const Faqs = () => {
                 <div className="w-full lg:w-1/2">
                     {AccordionItem.map((item, i) => (
                         <div className="">
-                            <div className="flex justify-between text-forest font-bold " onClick={() => setOpenIndex(openIndex === i ? null : i)}> {item.question} <span>{openIndex === i ? "-" : "+"}</span></div>
+                            <div className="flex justify-between text-forest font-bold " onClick={() => setOpenIndex(openIndex === i ? null : i)}>{item.question}<span>{openIndex === i ? "-" : "+"}</span></div>
                             <p>
                                 {openIndex === i ? item.answer : null}
                             </p>
