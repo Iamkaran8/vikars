2a5bc79fddc3ca69183ba8c20f4752bdfdea526a
thamarai <thamaraiselvig2109@gmail.com>
2025-09-25 15:04:54 +0530

    refactor : banner section done with responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,16 +21,14 @@ export default {
         '24' : "24px",
         '25' : "25px",
         '26' : "26px",
+        '28' : "28px",
         '32' : "32px",
         '36' : "36px",
         '38' : "38px",
         '40' : "40px",
         '48' : "48px",
         '64' : "64px",
-      },
-      fontFamily: {
-       inter: ['Inter', 'sans-serif'],
-      },
+      }
     },
   },
   plugins: [],
