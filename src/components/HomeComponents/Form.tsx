9844e75aa07a2933c5cb0fec504c66f825fcf307
Iamkaran8 <karanwebdev8@gmail.com>
2025-09-27 19:11:07 +0530

    form Fix

--- src/components/HomeComponents/Form.tsx
+++ src/components/HomeComponents/Form.tsx
@@ -28,13 +28,13 @@ export const Form = () => {
     }
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        fetch('https://script.google.com/macros/s/AKfycbyLMmW51omJ-GNSf3LbBOzgmWGQo4UFRof_YCadMD67hC5x_2FkKr4nvA__a1tN6vJJ/exec',{
-            method:'POST',
-            headers:{
-                "Content-Type" : "application/x-www-form-urlencoded",
-                body:(`Name=${formData.name}&Email=${formData.email}`)
-            }
-        }).then(res=>res.text())
+        fetch("https://script.google.com/macros/s/AKfycbyynMlNRw0gC4sxYQ1Gy03DzvywhbvnflshLY75tiu40U8hKEBRGndxinDjYI_km7yF/exec", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: `Name=${encodeURIComponent(formData.name)}&Email=${encodeURIComponent(formData.email)}&Board=${encodeURIComponent(formData.board)}&Location=${encodeURIComponent(formData.location)}&Phone=${encodeURIComponent(formData.phone)}`
+
+        });
+
         console.log("Form Submitted:", formData);
         alert(`Hello ${formData.name}, your details are saved!`);
     };
@@ -52,44 +52,46 @@ export const Form = () => {
 
     return (
 
-
-        <div className="bg-forest rounded-lg flex flex-col lg:flex-row  md:flex-col   pt-8 px-4">
-            <div className="p-8 gap-6 flex flex-col w-full lg:w-1/2 md:w-full">
-                <div className=" ">
-                    <h2 className="text-32 text-white font-semibold leading-tight">
-                        Book Your Free Online Demo Class See the Difference in Just 30 Minutes
-                    </h2>
-                    <p className="text-white font-light pt-2">
-                        Personalised 1:1 learning that adapts to your child’s pace. Watch doubts vanish and confidence grow.
-                    </p>
+        <div className="cont">
+            <div className="bg-forest rounded-lg flex flex-col lg:flex-row  md:flex-col   pt-8 px-4 py-4">
+                <div className="p-8 gap-6 flex flex-col w-full lg:w-1/2 md:w-full">
+                    <div className=" ">
+                        <h2 className="text-32 text-white font-semibold leading-tight">
+                            Book Your Free Online Demo Class See the Difference in Just 30 Minutes
+                        </h2>
+                        <p className="text-white font-light pt-2">
+                            Personalised 1:1 learning that adapts to your child’s pace. Watch doubts vanish and confidence grow.
+                        </p>
+                    </div>
+                    <div>
+                        <h3 className="bg-cream inline-flex text-center text-forest font-bold leading-tight p-4 rounded-lg">
+                            Hurry — limited demo slots available today!  Trusted by 5,000+<br /> parents across India.
+                        </h3>
+                        <p className="text-white pt-4">Bonus: Get a free detailed learning report + customised study plan after your trial</p>
+                    </div>
                 </div>
-                <div>
-                    <h3 className="bg-cream inline-flex text-center text-forest font-bold leading-tight p-4 rounded-lg">
-                        Hurry — limited demo slots available today!  Trusted by 5,000+<br /> parents across India.
-                    </h3>
-                    <p className="text-white pt-4">Bonus: Get a free detailed learning report + customised study plan after your trial</p>
-                </div>
-            </div>
 
-            <div className="w-full lg:w-1/2 md:w-full md:px-32">
-                <form onSubmit={handleSubmit} className="flex flex-col justify-center" >
+                <div className="w-full lg:w-1/2 md:w-full md:px-32">
+                    <form onSubmit={handleSubmit} className="flex flex-col justify-center" >
 
+                        <label className="text-white flex flex-col  ">
+                            Name:
 
-                    <label className="text-white flex flex-col ">
-                        Name:
+                        </label>
                         <input
-                            className="rounded p-2 text-black"
+                            className="rounded p-2 text-black text-20"
                             type="text"
                             name="name"
                             value={formData.name}
                             onChange={handleChange}
                             required
                         />
-                    </label>
-                    <br />
+                        <br />
 
-                    <label className="text-white flex flex-col ">
-                        Email:
+                        <label className="text-white flex flex-col ">
+                            Email:
+
+                        </label>
                         <input
                             className="rounded p-2 text-black"
 
@@ -99,35 +101,39 @@ export const Form = () => {
                             onChange={handleChange}
                             required
                         />
-                    </label>
-                    <br />
+                        <br />
+                        <div className="flex gap-4">
+                            <label className="text-white flex flex-col w-[48%]">
+                                Board:
+                                <input
+                                    className="rounded p-2 text-black"
+                                    type="text"
+                                    name="board"
+                                    value={formData.board}
+                                    onChange={handleChange}
+                                />
+                            </label>
 
-                    <label className="text-white flex flex-col ">
-                        Board:
-                        <input
-                            className="rounded p-2 text-black"
-                            type="text"
-                            name="board"
-                            value={formData.board}
-                            onChange={handleChange}
-                        />
-                    </label>
-                    <br />
 
-                    <label className="text-white flex flex-col ">
-                        Location:
-                        <input
-                            className="rounded p-2 text-black"
-                            type="text"
-                            name="location"
-                            value={formData.location}
-                            onChange={handleChange}
-                        />
-                    </label>
-                    <br />
+                            <label className="text-white flex flex-col w-[48%] ">
+                                Location:
+                                <input
+                                    className="rounded p-2 text-black"
+                                    type="text"
+                                    name="location"
+                                    value={formData.location}
+                                    onChange={handleChange}
+                                />
+                            </label>
 
-                    <label className="text-white flex flex-col ">
-                        Phone:
+                        </div>
+                        <br />
+
+                        <label className="text-white flex flex-col ">
+                            Phone:
+
+
+                        </label>
                         <input
                             className="rounded p-2 text-black"
                             type="text"
@@ -136,17 +142,20 @@ export const Form = () => {
                             onChange={handleChange}
                         />
                         <button className="bg-red-100 mt-4 p-2 rounded-[7px] text-black" onClick={() => sendOtp()} >Send OTP</button>
-                    </label>
-                    <div id="recaptcha"></div>
-                    <br />
+                        <div id="recaptcha"></div>
+                        <br />
 
-                    <button type="submit" className="flex justify-center bg-[#E95D5C] text-white py-1 px-8 rounded w-max ">Submit</button>
-                </form>
+                        <div className="flex items-center justify-center">
+                            <button type="submit" className="flex justify-center bg-[#E95D5C] text-white py-1 px-8 rounded w-max ">Submit</button>
+                        </div>
+                    </form>
 
+
+                </div>
 
             </div>
-
         </div>
+
     )
 }
 
